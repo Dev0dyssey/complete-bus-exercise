d29cf8046275bd8be82921e87d964dae1f89c95d
ThomasMartinUK <tmuk@protonmail.com>
2019-07-21 08:09:58 +0100

    Example eventBus exercise with complete code passing data between sibling components of the App

---
+++ src/components/Server/Servers.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <app-server
+                v-for="server in servers"
+                :server="server"></app-server>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import Server from './Server.vue';
+
+    export default {
+        data: function() {
+            return {
+              servers: [
+                  { id: 1, status: 'Normal', name: 'Gaming'},
+                  { id: 2, status: 'Critical', name: 'Accounts'},
+                  { id: 3, status: 'Unknown', name: 'Payroll'},
+                  { id: 4, status: 'Normal', name: 'Backup'}
+              ]
+            };
+        },
+        components: {
+            appServer: Server
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+export const serverBus = new Vue();
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  render: h => h(App)
+})
